0633485ec67508898701c173b3b74132f3cf2900
DigitalDreamerSW <sandexyn@poczta.fm>
2023-11-10 10:38:04 +0100

    dodanie opcji podawania parametru

--- cloud-init-pve.sh
+++ cloud-init-pve.sh
@@ -1,34 +1,55 @@
 #! /bin/bash
 #tworzenie nowego template na proxmoxie z wykorzystaniem obrazu cloud-init
+#dodac w przyszlosci opcje dodawania nazwy jako parametr i moze innych opcji
 
-#parametry maszyny wzorcowej
+#parametry maszyny wzorcowej do ewentualnej zmiany
 memory=2048
 cpu=2
-image_path="/mnt/cloud-init/debian-12-generic-amd64-20231013-1532.qcow2"
+image_path="/mnt/cloud-init/Rocky-9-GenericCloud.latest.x86_64.qcow2"
 name="debian-11"
 bridge_type="vmbr1"
 storage="local-zfs"
+
+#sprawdzanie czy podano parametr, parametrem powinien byc obraz
+if [ $# -eq 1 ] 
+then
+    if [ $1 == "--help" ]
+    then
+        echo "Skrypt mozna uruchomic z parametrem, jako parametr prosze podac obraz w formacie qcow2"
+        echo "Przyklad: $0 xyz.qcow2"
+        exit 0
+    else
+    image_path=$1
+        if [ ! -n "`file -b $image_path  |grep QCOW`" ]
+        then
+            echo "obraz nie jest w formacie qcow2 lub podano zly parametr"
+            exit 1
+        else
+            image_path=$(realpath $1) #musi byc pelna sciezka do pliku
+        fi
+    fi
+else
+    if [ -f $image_path ]
+    then
+        if [ ! -n "`file -b $image_path  |grep QCOW`" ]
+        then
+            echo "obraz nie jest w formacie qcow2"
+            exit 1
+        fi
+    else
+        echo "obraz nie istnieje lub podana zostala zla sciezka"
+        exit 1
+    fi
+fi
 
 #walidacja danych
 
 if [ $memory -ge 512 -a $memory -le 10240 ]
 then
-    if [ $cpu -ge 1 -a $cpu -le 4 ]
+    if [ $cpu -lt 1 -o $cpu -ge 4 ]
     then
-        if [ -f $image_path ]
-        then
-            if [ ! -n "`file -b $image_path  |grep QCOW`" ]
-            then
-                echo "obraz nie jest w formacie qcow2"
-                exit 1
-            fi
-        else
-            echo "obraz nie istnieje lub podana zostala zla sciezka"
-            exit 1
-        fi
-    else
-    echo "bledna wartosc cpu"
-    exit 1
+    echo "Podano bledna wartosc cpu"
+    exit 1 
     fi
 else
 echo "bledna wartosc ram"
@@ -44,6 +65,7 @@ create_vm()
 
 adjust_vm()
 {
+    sleep 2
     qm set $1 --scsi0 $storage:0,import-from=$image_path #importowanie dysku
     qm set $1 --ide2 $storage:cloudinit ## cd-rom cloud init
     qm set $1 --boot order=scsi0 
